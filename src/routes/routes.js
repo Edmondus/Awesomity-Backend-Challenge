4100f3d7d293f48e8211a34220dbe64ccc67062b
Edmond Ndayishimiye <ndedmondus97@gmail.com>
2021-10-17 11:51:35 +0200

    protecting the routes, add search suspend and activate features

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import EmployeeController from "../controllers/employeeController";
-import { verifyAge } from '../middlewares/auth';
+import { verifyAge,isAuthorized } from '../middlewares/auth';
 
 const router = express.Router();
 
@@ -10,5 +10,8 @@ router.post('/signup', verifyAge, employeeController.signup);
 router.post('/login', employeeController.signIn);
 router.get('/employees', employeeController.getAllEmployees);
 router.post('/employees/verify/:emp_code', employeeController.verifyEmail);
-router.delete('/employees/delete/:employee_code', employeeController.deleteEmployee);
+router.delete('/employees/delete/:employee_code',isAuthorized, employeeController.deleteEmployee);
+router.post('/employees/suspend/:employee_code', isAuthorized ,employeeController.suspendEmployee);
+router.post('/employees/activate/:employee_code', isAuthorized, employeeController.activateEmployee);
+router.get('/employees/search/:keyWord', employeeController.searchEmployee);
 export default router;
